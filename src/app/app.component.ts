21cc9e5deb148ec5afdb033f09d835b2809008b6
nicklefritz <nrcoover@gmail.com>
2026-05-15 12:54:39 -0400

    Setup simple conditional navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  destination = 'Recipes';
+
+  onSelectNavigation = (data: { destination: string }) => {
+    console.log(data.destination);
+    this.destination = data.destination;
+  };
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  @Output() selectNavigation = new EventEmitter<{ destination: string }>();
 
+  onRecipesClick = () => {
+    this.selectNavigation.emit({ destination: 'Recipes' });
+  };
+
+  onShoppingListClick = () => {
+    this.selectNavigation.emit({ destination: 'Shopping List' });
+  };
 }
