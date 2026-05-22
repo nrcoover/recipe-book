8a4ed2b87669aafb582eb40dbac061a3fe8a5d8b
nicklefritz <nrcoover@gmail.com>
2026-05-22 16:56:32 -0400

    Add functionality for adding new ingredients to the shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef | undefined;
+  @ViewChild('amountInput') amountInputRef: ElementRef | undefined;
 
-  constructor() { }
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  onAddItem() {
+    const ingredientName = this.nameInputRef?.nativeElement.value;
+    const ingredientAmount = this.amountInputRef?.nativeElement.value;
+    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+    this.ingredientAdded.emit(newIngredient);
   }
-
 }
